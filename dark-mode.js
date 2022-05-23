be8e218762dc4be496d6201250ca45f4e673df30
Luc0903 <87391029+Luc0903@users.noreply.github.com>
2022-05-22 23:07:46 -0300

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -2,10 +2,30 @@ const buttonDarkMode = document.querySelector('.header__icon');
 const topImage = document.querySelector('#top--image');
 
 buttonDarkMode.addEventListener('click', ()=> {
-
+	
 	document.body.classList.toggle('ligth');
-
-	if (document.body.classList.contains('ligth')) topImage.setAttribute('src', 'images/icon-moon.svg'); 
-	else topImage.setAttribute('src', 'images/icon-sun.svg');
+	classBlackMode();
 
 });
+
+function classBlackMode() {
+
+	if (document.body.classList.contains('ligth')) {
+		topImage.setAttribute('src', 'images/icon-moon.svg');
+		localStorage.setItem("modoOscuro", false);
+	} 
+	else {
+		topImage.setAttribute('src', 'images/icon-sun.svg');
+		localStorage.setItem("modoOscuro", true);
+	}
+
+}
+
+window.addEventListener("load", ()=> {
+	const modoOscuro = localStorage.getItem("modoOscuro");
+	
+	if (modoOscuro == "false") document.body.classList.add("ligth");
+	else document.body.classList.remove("ligth");
+	
+	classBlackMode();
+})
